a8e9c5f433cb5d142c6a0f713c94cca07d265bba
Furkan Demirci <151056854+dmrcfurkan@users.noreply.github.com>
2025-12-24 02:15:19 +0300

    Refactor Node class to use new attributes
    Updated Node class to include 'name' and 'properties' attributes, and modified methods accordingly.

--- src/node.py
+++ src/node.py
@@ -1,29 +1,7 @@
-
 class Node:
-    def __init__(self, id, ozelliklerMap=None):
-        self.kullaniciNo = id 
-        self.ozellikler = ozelliklerMap if ozelliklerMap is not None else {}
-        self.bagliKenarlar = []
+    def __init__(self, id, name, properties=None):
+        self.id = id
+        self.name = name
+        self.properties = properties or {}
+        self.neighbors = []
 
-    def baglantiKaydet(self, kenar):
-        self.bagliKenarlar.append(kenar)
-
-    def baglantiSil(self, kenarObjesi):
-        if kenarObjesi in self.bagliKenarlar:
-            self.bagliKenarlar.remove(kenarObjesi)
-
-    def nitelikDegistir(self, anahtar, yeniDeger):
-        self.ozellikler[anahtar] = yeniDeger
-
-    def raporBilgisi(self):
-        
-        metin = f"Kullanıcı No: {self.kullaniciNo}\n"
-        for k, v in self.ozellikler.items():
-            metin = metin + f"  - {k}: {v}\n"
-        return metin
-    
-    def __repr__(self):
-        return f"Kullanici(No={self.kullaniciNo})"
-    
-    #kullanıcı düğümünü temsil etmek için kullanılır.
-    #kullanıcı düğüme tıklayınca düğüme gösterilecek olan bilgiyi temsil eder
